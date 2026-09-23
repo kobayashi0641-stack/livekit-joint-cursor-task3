22fc0d57ccd43fec5fdd65d3092a1e510a335c1a
Toshiki Kobayashi <kobayashi0641@gmail.com>
2026-09-24 00:02:45 +0900

    Configure independent Task3 environments

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5175,
     host: '0.0.0.0',
     proxy: {
-      '/token': 'http://localhost:3001',
-      '/kick': 'http://localhost:3001',
-      '/latency-threshold': 'http://localhost:3001',
-      '/agent': 'http://localhost:3001',
+      '/token': 'http://localhost:3003',
+      '/kick': 'http://localhost:3003',
+      '/latency-threshold': 'http://localhost:3003',
+      '/agent': 'http://localhost:3003',
     },
   }
 });
